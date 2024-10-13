b685fb2989195eab4a98612d20f3f9d2024afa01
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-10-13 16:25:04 +0900

    정처기 준비

--- src/main/java/com/joney/shop/Domain/Item.java
+++ src/main/java/com/joney/shop/Domain/Item.java
@@ -17,6 +17,7 @@ public class Item {
     @Column(nullable = false)
     private String title;
 
+
     //20억까지만 저장이 가능한 Integer
     //좀 더 큰 숫자는 Long으로 사용 900경까지 사용가능
     //모든 곳에서 불러오기위해 public으로 우선사용. private으로 붙여도 되는데 getter,setter가 필요함
