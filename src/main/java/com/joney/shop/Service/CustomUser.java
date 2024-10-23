a94c5d198c7c6be4c6a29e88740b7918036e1f9f
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-10-23 15:12:23 +0900

    주문 기능 수정

--- src/main/java/com/joney/shop/Service/CustomUser.java
+++ src/main/java/com/joney/shop/Service/CustomUser.java
@@ -1,10 +1,14 @@
 package com.joney.shop.Service;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class CustomUser extends User {
 
     public Long id;
--- src/main/java/com/joney/shop/sales/SalesController.java
+++ src/main/java/com/joney/shop/sales/SalesController.java
@@ -1,11 +1,15 @@
 package com.joney.shop.sales;
 
+import com.joney.shop.Domain.Member;
+import com.joney.shop.Repository.MemberRepository;
 import com.joney.shop.Repository.SalesRepository;
 import com.joney.shop.Service.CustomUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,26 +20,34 @@ import java.util.List;
 public class SalesController {
 
     private final SalesRepository salesRepository;
+    private final MemberRepository memberRepository;
     @PostMapping("/order")
-    String OrderItems(@RequestParam String title, Integer price, Integer count, Authentication auth){
+    String OrderItems(@RequestParam String title,
+                      Integer price,
+                      Integer count,
+                      Authentication auth){
         Sales sales = new Sales();
         sales.setCount(count);
         sales.setPrice(price);
         sales.setItemName(title);
 
-        CustomUser user = (CustomUser) auth.getPrincipal();
+        CustomUser customUser = (CustomUser) auth.getPrincipal();
 //        System.out.println(user.id);
-//        sales.setMemberId(user.id);
+        Member member = memberRepository.findById(customUser.getId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다.")
+        );
+        sales.setMember(member);
 
         salesRepository.save(sales);
 
-        return "list.html";
+        return "redirect:list?success=true";
     }
 
     @GetMapping("/order/all")
-    String getOrderAll(){
+    String getOrderAll(Model model){
         List<Sales> result = salesRepository.findAll();
-        System.out.println(result.get(1));
-        return "list.html";
+//        System.out.println(result.get());
+        model.addAttribute("orders",result);
+        return "OrderList.html";
     }
 }
