87846af706cd048bbd81ec5b2861f55133a80073
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-10-24 21:50:09 +0900

    ManyToOne의 단점, 디비에 무리가 갈 수 있다, 쓸모없는 정보들까지 전부 다 보내버린다. -> JPQL를 사용해서 JOIN문을 통해 한줄로 바꿔서 디비를 쾌적하게 해보자. DTO를 사용해서 원하는 정보만 전달해보자.

--- src/main/java/com/joney/shop/Repository/SalesRepository.java
+++ src/main/java/com/joney/shop/Repository/SalesRepository.java
@@ -2,6 +2,13 @@ package com.joney.shop.Repository;
 
 import com.joney.shop.sales.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SalesRepository extends JpaRepository<Sales, Long> {
+
+    //join문법을 쌩으로 작성하기보다는 JPQL로 작성하면 더 수월하게 작업가능
+    @Query(value = "SELECT s FROM Sales s JOIN FETCH s.member")
+    List<Sales> customFindAll();
 }
--- src/main/java/com/joney/shop/sales/SalesController.java
+++ src/main/java/com/joney/shop/sales/SalesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -43,11 +44,37 @@ public class SalesController {
         return "redirect:list?success=true";
     }
 
+
+    //ManyToOne을 쓰면 편리하긴 하지만 N+1의 문제가 발생할 수 있다.
+    //JOIN문법으로 해결해보자.
+    //JPQL문법으로 행만큼 실행되던 sql을 단 한번으로 해결했음.
+
+    //ManyToOne을 사용하면 패스워드도 함께 가져오는데..? RestAPI로 보낸다면 참사가 일어날 것 같음
+    //Map이나 Dto를 만들어서 원하는 정보만 보내보자.
     @GetMapping("/order/all")
     String getOrderAll(Model model){
-        List<Sales> result = salesRepository.findAll();
+        List<Sales> result = salesRepository.customFindAll();
 //        System.out.println(result.get());
+//        var salesDto = new SalesDto();
+//        salesDto.itemName = result.get(0).getItemName();
+
+        List<SalesDto> salesDtoList = result.stream()
+                .map(sales -> new SalesDto(sales.getItemName(), sales.getPrice(), sales.getMember().getUsername()))
+                .collect(Collectors.toList());
+
         model.addAttribute("orders",result);
         return "OrderList.html";
+    }
+
+    public class SalesDto{
+        String itemName;
+        Integer price;
+        String username;
+
+        public SalesDto(String itemName, Integer price, String username){
+            this.itemName = itemName;
+            this.price = price;
+            this.username = username;
+        }
     }
 }
